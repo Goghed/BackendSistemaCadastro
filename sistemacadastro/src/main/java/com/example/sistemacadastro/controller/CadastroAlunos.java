fcf0abd922083227f230bd70462e83d531df3e58
ED CARLOS DA SILVA PEREIRA <63868640+Goghed@users.noreply.github.com>
2021-12-13 21:22:49 -0300

    update 8

--- sistemacadastro/src/main/java/com/example/sistemacadastro/controller/CadastroAlunos.java
+++ sistemacadastro/src/main/java/com/example/sistemacadastro/controller/CadastroAlunos.java
@@ -9,6 +9,7 @@ import com.example.sistemacadastro.repository.EstudanteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +24,6 @@ public class CadastroAlunos {
     private EstudanteRepository repo;
 
     // End Point 1
-
-
     // (GET) http://localhost:8080/estudantes
     @GetMapping
     public List <Estudante> getEstudantes(){
@@ -33,5 +32,13 @@ public class CadastroAlunos {
 
         return list;
     }
-    
+
+    // End Point 2
+    // (GET) http://localhost:8080/estudantes/{id}
+    @GetMapping("{id}")
+    public Estudante getEstudante(@PathVariable int id){
+        Estudante e = repo.findById(id).get;
+
+        return h;
+    }
 }
