0a98a05a61d8ab939ae54018cfaf54b14ff04f0b
ED CARLOS DA SILVA PEREIRA <63868640+Goghed@users.noreply.github.com>
2021-12-13 21:12:01 -0300

    update 7

--- sistemacadastro/src/main/java/com/example/sistemacadastro/controller/CadastroAlunos.java
+++ sistemacadastro/src/main/java/com/example/sistemacadastro/controller/CadastroAlunos.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.example.sistemacadastro.entity.Estudante;
+import com.example.sistemacadastro.repository.EstudanteRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,37 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class CadastroAlunos {
 
 
-
+    // Injeta uma dependencia !!!
+    @Autowired
+    private EstudanteRepository repo;
 
     // End Point 1
-    
+
 
     // (GET) http://localhost:8080/estudantes
     @GetMapping
     public List <Estudante> getEstudantes(){
 
-        Estudante e1 = new Estudante();        
-        e1.setId(10);
-        e1.setNome("Ed Carlos");
-
-        Estudante e2 = new Estudante();        
-        e2.setId(10);
-        e2.setNome("Ed Carlos");
-
-        Estudante e3 = new Estudante();        
-        e3.setId(10);
-        e3.setNome("Ed Carlos");
-
-        Estudante e4 = new Estudante();        
-        e4.setId(10);
-        e4.setNome("Ed Carlos");
-
-        ArrayList <Estudante> list = new ArrayList<>();
-
-        list.add(e1);
-        list.add(e2);
-        list.add(e3);
-        list.add(e4);
+        List <Estudante> list = repo.findAll();
 
         return list;
     }
