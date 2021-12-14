22a0e86a81d791d32695c82f88e7fc13b0bcb12b
ED CARLOS DA SILVA PEREIRA <63868640+Goghed@users.noreply.github.com>
2021-12-13 21:44:40 -0300

    update 10

--- sistemacadastro/src/main/java/com/example/sistemacadastro/controller/CadastroAlunos.java
+++ sistemacadastro/src/main/java/com/example/sistemacadastro/controller/CadastroAlunos.java
@@ -2,18 +2,23 @@ package com.example.sistemacadastro.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.example.sistemacadastro.entity.Estudante;
 import com.example.sistemacadastro.repository.EstudanteRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/estudantes")
@@ -51,5 +56,23 @@ public class CadastroAlunos {
         return estudante;
     }
 
+    // End Point 4
+    // Remove um estudante, é necessário passar o id
+    @DeleteMapping("{id}")
+    public void remover(@PathVariable Integer id){
+        Optional<Estudante> op = repo.findById(id);
+        Estudante estudante = op.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        repo.delete(estudante);
+    }
 
+    //EndPoint 5
+    //Altera um estudante, é necessário passar os dados do heroi no formato JSON e tambem o id
+    @PutMapping("{id}")
+    public Estudante alterar(@RequestBody Estudante updateEstudante, @PathVariable Integer id){
+        Optional<Estudante> op =  repo.findById(id);
+        Estudante estudante = op.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        estudante.setNome(updateEstudante.getNome());
+        repo.save(estudante);
+        return estudante;
+    }
 }
